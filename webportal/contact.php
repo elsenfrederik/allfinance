06e48db2fe29c713afd81c2c02ba8cdfa9f84594
Frederik.Elsen <frederik.elsen@gfk.com>
2019-01-14 11:58:23 +0100

    first commit

---
+++ webportal/contact.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="shortcut icon" href="http://www.allfinance.be/All_Finance.ico">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="All Finance, Kredieten, Leningen, Krediet Aanvragen, Leonardstraat 16 - Leopoldsburg, Liliane Ten Grootenhuyzen, kortlopende leningen, langlopende leningen, financieringen, leasingen, kredieten: leningen, kortlopende leningen, langlopende leningen, financieringen, leasingen, overbruggingskredieten, eigenaarskredieten, hypotheken" />
+<title>All Finance BVBA</title>
+<link href="test.css" type="text/css" rel="stylesheet"/>
+<style type="text/css">
+<!--
+.stijl1 {
+	color: #000000;
+	font-size: 20px;
+	font-style: italic;
+}
+.stijl27 {
+	font-size: 10px;
+	color: #FFFF00;
+	font-family: arial;
+	line-height: 20px;
+	letter-spacing: 0.1em;
+}
+a:hover {
+	color: #FFFF00;
+	text-decoration: none;
+}
+-->
+</style>
+<script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+<!--
+.stijl36 {color: #000000}
+a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+.stijl53 {font-style: normal; font-weight: normal; font-family: arial; font-size: 12px;}
+.stijl44 {font-size: 14px}
+.stijl59 {line-height: 20px; letter-spacing: 0.1em; color: #FF0000; font-family: "Comic Sans MS"; font-size: medium; }
+.stijl62 {font-size: 12px}
+.stijl63 {	color: #000000;
+	font-size: 20px;
+}
+.stijl55 {	font-size: 13px
+}
+.stijl64 {font-size: 11px; }
+.stijl65 {
+	color: #FF0000;
+	font-size: 36px;
+}
+.stijl3 {	color: #FFFFFF;
+	font-size: 12px;
+	font-style: normal;
+	font-weight: normal;
+	font-family: arial;
+}
+.stijl66 {color: #333333}
+.stijl67 {color: #000000; font-size: 12px; }
+-->
+</style>
+<style>
+.small{
+	width:50px;
+	margin-right:5px;}
+table{
+	text-align:left;
+	font-size:12px;
+	}
+td{
+	
+	vertical-align:top;}
+.three, .four{
+	width:900px;
+	border:1px solid black;
+	margin-bottom:10px;}
+.three .first{
+	width:200px;}
+.four td{
+	width:25%;}
+.smalltable input{
+	width:50px;
+	padding:0px;
+	margin:0px;}
+
+</style>
+</head>
+<body oncontextmenu="return false;" ondragstart="return false;" onselectstart="return false; document.getElementById('loading').style.display = 'none'; reveal();">
+<body bgcolor="#0DA1E7" background="wb_01.gif">
+
+<table width="900" height="215%" align="center" bgcolor="#FAFBFB" bordercolor="#FAFBFB">
+<tr>
+            <th scope="col" height="10"><table width="100%" height="114" border="0" align="center" cellpadding="0" cellspacing="0" bordercolor="#0033FF">
+            <tr align="center">
+                  <th height="80" bordercolor="#993300" class="stijl27" scope="col"><img src="All_Finance_BVBA_Banner.jpg" width="898" height="80" hspace="0" vspace="5" />
+                    <ul id="Home" class="MenuBarHorizontal">
+                      <li>
+                        <div align="center" class="stijl44">
+                          <div align="center"><a href="index.html" target="_self" class="stijl64">Home</a></div>
+                        </div>
+                      </li>
+                      <li>
+                        <div align="center" class="stijl64"><a href="Historie.html" target="_self">Historie</a></div>
+                      </li>
+                      <li>
+                        <div align="center" class="stijl64"><a href="Leningen.html" target="_self">Leningen</a></div>
+                  </li>
+                      <li>
+                        <div align="center" class="stijl64"><a href="Hypotheken.html" target="_self">Hypotheken</a></div>
+                      </li>
+                      <li>
+                        <div align="center" class="stijl64"><a href="Krediet_Aanvragen.html" target="_self">Krediet aanvragen</a></div>
+                      </li>
+              </ul>                    
+                    <ul id="MenuBar2" class="MenuBarHorizontal">
+                      <li>
+                        <div align="center" class="stijl64"><a href="mailto:lilianetg@hotmail.com" target="_blank">Contact</a></div>
+                      </li>
+              </ul>              </th>
+              </tr>
+    </table> 
+<form action="contact_submit.php" method="post">
+<table class="three">
+<tr><td colspan="3"> <span class="stijl63"><em>Krediet aanvragen</em></span><p align="left"><span class="stijl59">Geheel vrijblijvende aanvraag voor een lening op afbetaling</span></p>
+                      <p align="right"><span class="stijl64 stijl65">Bel mij voor info, ik help u! 011 39 29 49</span></p></td>
+</table>
+<table class="three">
+<tr>
+<td class="first">Gewenst bedrag: </td><td colspan="2"><input name="gewenst_bedrag" type="text" />
+Doel: 
+<select name="doel"><option value="lening op afbetaling">Lening op afbetaling</option><option value="Hypothecaire lening">Hypothecaire lening</option></select></td></tr>
+</table>
+<table class="three">
+<tr><td class="first">Uw gegevens</td><td class="second">Ontlener</td><td class="third">Mede-ontlener</td></tr>
+<tr><td>Telefoonnummer</td><td><input type="text" name="tel_ontlener" /></td><td><input name="tel_mede_ontlener" type="text" /></td></tr>
+<tr><td>Naam<td><input type="text" name="naam_ontlener" /></td><td><input name="naam_mede_ontlener" type="text" /></td></tr>
+<tr><td>Voornaam</td><td><input type="text" name="voornaam_ontlener" /></td><td><input name="voornaam_mede_ontlener" type="text" /></td></tr>
+<tr><td>Geboortedatum (dd - mm - jjjj)</td><td><input class="small" type="text" maxlength="2" name="dag_ontlener" /><input class="small" maxlength="2" type="text" name="maand_ontlener" /><input class="small" maxlength="4" type="text" name="jaar_ontlener" /></td><td><input class="small" type="text" maxlength="2" name="dag_mede_ontlener" /><input class="small" maxlength="2" type="text" name="maand_mede_ontlener" /><input maxlength="4" class="small" type="text" name="jaar_mede_ontlener" /></td></tr>
+<tr><td>Adres</td><td colspan="2"><input type="text" name="adres" /> Nr: <input class="small" type="text" name="huisnummer" />Bus: <input class="small" type="text" name="bus" /></td></tr>
+<tr><td>Postcode &amp; Plaats</td><td colspan="2"><input type="text" class="small" name="postcode" maxlength="4" /><input type="text" name="plaats" />  </td></tr>
+<tr><td>Rijksregisternummer<td><input type="text" name="rijksregister_ontlener" /></td><td><input name="rijksregister_mede_ontlener" type="text" /></td></tr>
+
+</table>
+
+<table class="three">
+<tr><td class="first">Burgerlijke staat<br />Vink het passende vakje aan a.u.b.</td>
+<td class="second">
+<input type="radio" name="burgerlijke_staat_ontlener" value="Vrijgel" /> vrijgezel<br />
+<input type="radio" name="burgerlijke_staat_ontlener" value="gehuwd" /> gehuwd<br />
+<input type="radio" name="burgerlijke_staat_ontlener" value="gescheiden" /> gescheiden<br />
+<input type="radio" name="burgerlijke_staat_ontlener" value="gescheiden_van_tafel_en_bed" /> Gescheiden van tafel en bed<br />
+<input type="radio" name="burgerlijke_staat_ontlener" value="weduwe - weduwnaar" /> weduwe - weduwnaar<br />
+<input type="radio" name="burgerlijke_staat_ontlener" value="samenwonend" checked="checked" /> samenwonend
+</td>
+<td class="third">
+<input type="radio" name="burgerlijke_staat_mede_ontlener" value="Vrijgel" /> vrijgezel<br />
+<input type="radio" name="burgerlijke_staat_mede_ontlener" value="gehuwd" /> gehuwd<br />
+<input type="radio" name="burgerlijke_staat_mede_ontlener" value="gescheiden" /> gescheiden<br />
+<input type="radio" name="burgerlijke_staat_mede_ontlener" value="gescheiden_van_tafel_en_bed" /> Gescheiden van tafel en bed<br />
+<input type="radio" name="burgerlijke_staat_mede_ontlener" value="weduwe - weduwnaar" /> weduwe - weduwnaar<br />
+<input type="radio" name="burgerlijke_staat_mede_ontlener" value="samenwonend" checked="checked" /> samenwonend
+</td>
+
+</tr>
+</table>
+<table class="three">
+<tr>
+<td class="first">
+Uw maandelijks nettoinkomen
+</td>
+<td  class="second">
+<table class="smalltable">
+<tr><td><input type="checkbox" name="salaris_ontlener" /> Salaris </td><td><input type="text" name="salaris_ontlener_value" /> &euro;/maand</td></tr>
+<tr><td><input type="checkbox" name="werkloosheidsuitkering_ontlener" /> Werkloosheidsuitkering </td><td><input type="text" name="werkloosheidsuitkering_ontlener_value" /> &euro;/maand</td></tr>
+<tr><td><input type="checkbox" name="pensioen_ontlener" /> Pensioen </td><td><input type="text" name="pensioen_ontlener_value" /> &euro;/maand </td></tr>
+<tr><td><input type="checkbox" name="invaliditeitsuitkering_ontlener" /> Invaliditeitsuitkering </td><td><input type="text" name="invaliditeitsuitkering_ontlener_value" /> &euro;/maand</td></tr>
+<tr><td><input type="checkbox" name="kinderbijslag_ontlener" /> Kinderbijslag  </td><td><input type="text" name="kinderbijslag_ontlener_value" /> &euro;/maand</td></tr>
+<td><input type="checkbox" name="andere_ontlener" /> Andere <br /><br />Detail: <br /><input type="text" name="detail_ontlener" style="width:200px;" /></td><td><input type="text" name="andere_ontlener_value" /> &euro;/maand</td>
+</table>
+
+</td>
+
+<td class="third">
+<table class="smalltable">
+<tr><td><input type="checkbox" name="salaris_mede_ontlener" /> Salaris </td><td><input type="text" name="salaris_mede_ontlener_value" /> &euro;/maand</td></tr>
+<tr><td><input type="checkbox" name="werkloosheidsuitkering_mede_ontlener" /> Werkloosheidsuitkering </td><td><input type="text" name="werkloosheidsuitkering_mede_ontlener_value" /> &euro;/maand</td></tr>
+<tr><td><input type="checkbox" name="pensioen_mede_ontlener" /> Pensioen </td><td><input type="text" name="pensioen_mede_ontlener_value" /> &euro;/maand</td>
+</tr>
+<tr><td><input type="checkbox" name="invaliditeitsuitkering_mede_ontlener" /> Invaliditeitsuitkering </td><td><input type="text" name="invaliditeitsuitkering_mede_ontlener_value" /> &euro;/maand</td></tr>
+<tr><td><input type="checkbox" name="kinderbijslag_mede_ontlener" /> Kinderbijslag </td><td><input type="text" name="kinderbijslag_mede_ontlener_value" /> &euro;/maand</td></tr>
+
+<tr><td><input type="checkbox" name="andere_mede_ontlener" /> Andere <br /><br />Detail: <br /><input type="text" name="detail_ontlener" style="width:200px;" /></td><td><input type="text" name="andere_mede_ontlener_value" /> &euro;/maand</td></tr>
+
+</table>
+</td>
+</tr>
+</table>
+
+<table class="four">
+<tr><td rowspan="4">Andere kredieten in omloop<br  />(hypothecair krediet inbegrepen)<br /><br /><input type="radio" value="krediet_ja" name="krediet" />Ja<br /><input type="radio" value="krediet_neen" name="krediet" />Neen</td>
+<td class="bold">Maatschappij(en)</td>
+<td class="bold">Geleend bedrag</td>
+<td class="bold">Duurtijd</td>
+</tr>
+<tr>
+<td><input type="text" name="maatschappij1" /><br /><input type="text" name="maatschappij2" /><br /><input type="text" name="maatschappij3" /><br /><input type="text" name="maatschappij4" /></td>
+<td><input type="text" name="geleendbedrag1" style="width:150px;"  /> &euro;<br /><input type="text" name="geleendbedrag2" style="width:150px;"  /> &euro;<br /><input type="text" style="width:150px;"  name="geleendbedrag3" /> &euro;<br /><input type="text" style="width:150px;" name="geleendbedrag4" /> &euro;</td>
+<td>
+<input type="text" name="looptijd1" /> maand<br /><input type="text" name="looptijd2" /> maand<br /><input type="text" name="looptijd3" /> maand<br /><input type="text" name="looptijd4" /> maand
+</td>
+</tr>
+<tr>
+<td class="bold">Maandelijkse termijnen</td>
+<td class="bold">In omloop sinds</td>
+<td class="bold">Terug te betalen?</td>
+</tr>
+<tr>
+<td><input type="text" name="termijnen1" /> &euro;<br /><input type="text" name="termijnen2" /> &euro;<br /><input type="text" name="termijnen3" /> &euro;<br /><input type="text" name="termijnen4" /> &euro;</td>
+<td>
+<input class="small" type="text" maxlength="2" name="omloop1_sinds_dag" />/
+<input class="small" maxlength="2" type="text" name="omloop1_sinds_maand" />/
+<input maxlength="4" class="small" type="text" name="omloop1_sinds_jaar" /><br />
+<input class="small" type="text" maxlength="2" name="omloop2_sinds_dag" />/
+<input class="small" maxlength="2" type="text" name="omloop2_sinds_maand" />/
+<input maxlength="4" class="small" type="text" name="omloop2_sinds_jaar" /><br />
+<input class="small" type="text" maxlength="2" name="omloop3_sinds_dag" />/
+<input class="small" maxlength="2" type="text" name="omloop3_sinds_maand" />/
+<input maxlength="4" class="small" type="text" name="omloop3_sinds_jaar" /><br />
+<input class="small" type="text" maxlength="2" name="omloop4_sinds_dag" />/
+<input class="small" maxlength="2" type="text" name="omloop4_sinds_maand" />/
+<input maxlength="4" class="small" type="text" name="omloop4_sinds_jaar" /><br /></td>
+<td>
+<input type="text" name="tebetalen1" /> &euro;<br /><input type="text" name="tebetalen2" /> &euro;<br /><input type="text" name="tebetalen3" /> &euro;<br /><input type="text" name="tebetalen4" /> &euro;
+</td>
+</tr>
+
+</table>
+
+
+<table class="three">
+<tr><td class="first">Uw werkgever of betalende instantie</td>
+<td class="second" style="vertical-align:bottom;"><input type="text" name="naam_betalendeinstantie_ontlener" style="width:90%;" /></td>
+<td class="third" style="vertical-align:bottom;"><input style="width:90%;" type="text" name="naam_betalendeinstantie_mede_ontlener" /></td></tr>
+<tr><td class="first">Plaats</td>
+<td class="second"><input style="width:90%;" type="text" name="adres_betalendeinstantie_ontlener" /></td>
+<td class="third"><input style="width:90%;" type="text" name="adres_betalendeinstantie_mede_ontlener" /></td></tr>
+<tr><td class="first">Sinds (dd mm jjjj)</td>
+<td class="second"><input class="small" type="text" maxlength="2" name="sinds_dag_ontlener" /><input class="small" maxlength="2" type="text" name="sinds_maand_ontlener" /><input maxlength="4" class="small" type="text" name="sinds_jaar_ontlener" /></td>
+<td class="third"><input class="small" type="text" maxlength="2" name="sinds_dag_mede_ontlener" /><input class="small" maxlength="2" type="text" name="sinds_maand_mede_ontlener" /><input maxlength="4" class="small" type="text" name="sinds_jaar_mede_ontlener" /></td></tr>
+<tr><td class="first"><strong>BENT U</strong><br />Vink het passende vakje aan aub.</td>
+<td class="second"><input type="radio" checked="checked" value="huurder" name="woonsituatie_huur" /> huurder <input type="text" name="huurder_value" class="small" /> &euro;/maand<br /><input type="radio" name="woonsituatie_huur" value="gratis inwonend" /> gratis inwonend</td>
+<td class="third"><input type="radio" name="woonsituatie_verhuur" value="eigenaar" /> eigenaar <input type="text" name="verhuurder_value" class="small" /> &euro;/maand<br /><input type="radio" name="woonsituatie_verhuur" checked="checked" value="geen eigenaar" /> geen eigenaar</td>
+</tr>
+<tr><td colspan="3"><input type="submit" value="Formulier versturen" name="submitform" /></td>
+
+</table>
+
+
+
+
+
+</form>
+       <p align="center"><span class="stijl3 stijl36"><span class="stijl66">Copyright (C) 2008-2013 All Finance BVBA All rights Reserved.</span></span></p>
+                      </td>
+                  </tr>
+                </table>
+                  <div align="center" class="stijl36"><span class="stijl53">
+                  </span></div>                </th>
+              </tr>
+            </table></td>
+</tr></table>
+</form>
+<script type="text/javascript">
+<!--
+//-->
+</script>
+</body>
+</html>
